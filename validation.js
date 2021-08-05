7ae8b515890cd9118c147e582e4e7c7c2b4b2e64
mbdesigns1989 <mbdesigns1989@yahoo.com>
2021-08-05 11:07:48 +0300

    Modified validation.js

--- validation.js
+++ validation.js
@@ -9,4 +9,13 @@ email.addEventListener('focus', () => {
 msgBox.textContent = 'Please make sure the email is all lower case';
 msgBox.style.color = '#fff';
 
- 
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
+    e.preventDefault();
+    msgBox.style.opacity = '1';
+    email.style.outline = 'solid rgba(255, 97, 99, 1)';
+    setTimeout(() => {
+      msgBox.style.opacity = '0';
+    }, 5000);
+  }
+});
