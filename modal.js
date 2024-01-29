7c78af17cf9814d8394b51f9c5bbf95a67216f68
Mohamed Badr <83557149+mbdesigns1989@users.noreply.github.com>
2024-01-29 09:58:06 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -32,19 +32,18 @@ const populateModal = (index) => {
       githubLink: 'https://github.com/mbdesigns1989/awesome-books',
     },
     {
-      projectName: 'Tonic',
-      canopy: 'CANOPY',
-      role: 'Back End Dev',
-      year: '2015',
+      projectName: 'JS-Capstone-Project',
+      role: 'front End Dev',
+      year: '2021',
       image: {
         URL: './assets/images/project3.png',
-        alt: 'My third project snapshot',
+        alt: 'JS-Capstone-project snapshot',
       },
       description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. ',
-      technologies: ['html', 'css', 'javascipt', 'bootstrap', 'github', 'ruby'],
-      liveLink: 'https://mbdesigns1989.github.io/my-portfolio-website/',
-      githubLink: 'https://github.com/mbdesigns1989/my-portfolio-website',
+        'An API based web app that displays Meals from the MealsDB API and the Involvement API from Microverse. The features of this webapp include an option to like a meal by clicking on the like button A comment button that opens up a modal and allows users to leave comments.',
+      technologies: ['html', 'css', 'javascipt'],
+      liveLink: 'https://effulgent-dodol-e273cf.netlify.app/',
+      githubLink: 'https://github.com/mbdesigns1989/JS-Capstone-Project-',
     },
     {
       projectName: 'Multi-Post Stories',
