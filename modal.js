081e121629e10b87b92f972e7a4cf9d38285b06e
Mohamed Badr <83557149+mbdesigns1989@users.noreply.github.com>
2024-01-29 09:49:08 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -18,19 +18,18 @@ const populateModal = (index) => {
       githubLink: 'https://github.com/mbdesigns1989/To-do-List',
     },
     {
-      projectName: 'Multi-Post Stories',
-      canopy: 'CANOPY',
-      role: 'Back End Dev',
-      year: '2015',
+      projectName: 'Awesome Books',
+      role: 'Front End Dev',
+      year: '2021',
       image: {
         URL: './assets/images/project2.png',
-        alt: 'My second project snapshot',
+        alt: 'Awesome Books project snapshot',
       },
       description:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent. ',
-      technologies: ['html', 'css', 'javascipt', 'bootstrap', 'github', 'ruby'],
-      liveLink: 'https://mbdesigns1989.github.io/my-portfolio-website/',
-      githubLink: 'https://github.com/mbdesigns1989/my-portfolio-website',
+        'Awesome books is a simple website that displays a list of books and allows you to add and remove books from that list.',
+      technologies: ['html', 'css', 'javascipt'],
+      liveLink: 'https://mbdesigns1989.github.io/awesome-books/',
+      githubLink: 'https://github.com/mbdesigns1989/awesome-books',
     },
     {
       projectName: 'Tonic',
